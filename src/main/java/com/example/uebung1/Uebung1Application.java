3a319bc7f003f634b658bad7520c20c748829fe0
Nazia <wi21b122@technikum-wien.at>
2022-06-21 13:24:32 +0200

    Initial commit

---
+++ src/main/java/com/example/uebung1/Uebung1Application.java
@@ -0,0 +1,13 @@
+package com.example.uebung1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uebung1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uebung1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uebung1/Uebung1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uebung1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uebung1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
